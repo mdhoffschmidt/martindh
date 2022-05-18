938ce5154e9a72625c38a6594daba8a416a658f8
Martin d'Hoffschmidt <mdhoffschmidt@protonmail.com>
2022-05-18 12:09:00 +0200

    update about

--- src/layouts/home.js
+++ src/layouts/home.js
@@ -7,19 +7,27 @@ export default function Home() {
             </h1>
 
             <p>
-                I am Lead Data Scientist at ILLUIN Technology, Paris. 
-                We bridge the gap between applied research and industry business challenges. 
-                We strive to understand, implement and put into production cutting-edge AI solutions in the Natural Language Processing field.
+                I'am currently building a Web3 project with the aim to solve data quality issues in the Data Science Landscape.
+                On the one side, my past experience in Data Science and Data Engineering made me realize how important data quality is to successfuly apply AI in the real world.
+                On the other side, it is back in 2016 that I heard for the first time the word blockchain and have since then closely watched in space mature.
+                It is about time to make it work for real applications.
             </p>
 
             <p>
+                Previously, I was Lead Data Sciencist at ILLUIN Technology, an AI start up based in Paris.
+                What we do the very best at ILLUIN is deploying Machine Learning models for the real world.
+                Apart from technical challenges, leading the data science team taught me important skills such as recruiting, business development and R&D Roadmap planing.
+            </p>
+
+            {/* <p>
                 Previously, I gained experience in the field of real time and distributed optimisation of energy systems working in collaboration with École Polytechnique Federale de Lausanne. 
                 I have also co-founded a Deep Learning based Crypto-currency fund to develop new approaches of algorithmic trading.
-            </p>
+            </p> */}
 
             <p>
-                I graduated with a Master Degree in Mechanical Engineering from École Polytechnique Fédérale de Lausanne with a major in Control Systems and Optimisation and a minor in Energy. 
-                I accomplished my Master Thesis at UC Berkeley in Deep Learning for curiosity driven agents.
+                I graduated with a Master Degree in Mechanical Engineering from École Polytechnique Fédérale de Lausanne with a major in Control Systems and Optimisation and a minor in Energy Engineering.
+                I accomplished my Master Thesis at UC Berkeley working on the implementation of Curiosity driven Agents applied to Autonomous Race Driving using Deep Learning.
+                It is there that I got addicted in building in understanding Neural Networks, building them from scratch in C to make them work in Micro Controlers.
             </p>
 
         </div>
