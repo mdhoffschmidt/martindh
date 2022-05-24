6e77130f36e83b3c9826f42af03ab8ec20f0f847
Martin d'Hoffschmidt <mdhoffschmidt@protonmail.com>
2022-05-24 16:56:56 +0200

    finalize article 1

---
+++ src/components/horizontalDivider.js
@@ -0,0 +1,9 @@
+export default function HorizontalDivider() {
+    return (
+        <div className="relative">
+            <div className="absolute inset-0 flex items-center" aria-hidden="true">
+                <div className="w-full border-t border-gray-100" />
+            </div>
+        </div>
+    )
+}
--- src/layouts/page.js
+++ src/layouts/page.js
@@ -2,16 +2,6 @@ import Navbar from "./navbar";
 import Footer from "./footer";
 
 
-function HorizontalDivider() {
-    return (
-        <div className="relative">
-            <div className="absolute inset-0 flex items-center" aria-hidden="true">
-                <div className="w-full border-t border-gray-100" />
-            </div>
-        </div>
-    )
-}
-
 export default function Page(props) {
 
     return (
@@ -23,15 +13,11 @@ export default function Page(props) {
                 </div>                     
             </header>
 
-            {/* <HorizontalDivider/> */}
-
             <main>
                 <div className="divMain">
                     { props.main }
                 </div>
             </main>
-
-            {/* <HorizontalDivider/> */}
 
             <footer>
                 <div className="divFooter">
