e1f49fb8dbb47e218fcded0ad47d9b24c073ae8c
Martin d'Hoffschmidt <mdhoffschmidt@protonmail.com>
2022-09-05 14:46:30 +0200

    update article 2022-08-19 with new sentences and typo corrections

--- src/articles/article-2022-08-19.js
+++ src/articles/article-2022-08-19.js
@@ -7,10 +7,15 @@ export const Meta = {
 export const Abstract = () => {
     return (
         <div>
-            Web3 users like the decentralized nature of their assets.
-            However, bying or exchanging them relies on centralized platforms.
-            It is the case for instance for NFT users that exchange their assets through platform like OpenSea, Rarible or SuperRare.
-            We analyse here the web traffic on five of the most popular platforms to understand what device user use, their gender and age and what social platforms they usualy use.
+            Blockchain users like the decentralized nature of the assets they hold.
+            To exchange them, they rely however on centralized platforms.
+            NFT users use platforms like <a href="https://opensea.io/"> OpenSea </a>, <a href="https://rarible.com/"> Rarible </a> or <a href="https://superrare.com/"> SuperRare </a>.
+            We analyse here the web traffic of five of the most popular platforms.
+            First, OpenSea is the clear leader in terms of traffic with 88% of it.
+            Second, Twitter provides 75% of the social traffic of these platforms.
+            Third, the users are mostly men, nearly 70%, and 80% are less than 45 years old.
+            Many more numbers are availble in the present article that gathers all the details you need.
+            Hopefully it will be useful to other builders.
         </div>
     );
 }
@@ -24,21 +29,27 @@ export const Article = () => {
             </h1>
 
             <p>
-                We look at free data provided by Similarweb from five of the most popular NFT exchange platforms.
-                The platforms considered are the following :
+                Who are the NFT users ? How old are they ?
+                What social media do they use ?
+                The present article attempts to give an answer to these questions.
+                NFT users exchange their assets on platforms like <a href="https://opensea.io/"> OpenSea </a> or <a href="https://superrare.com/"> SuperRare </a>.
+                These platforms can be monitored in terms of traffic to get insight about the user and answer the above mentionned questions.
             </p>
 
-            <ol>
-                <li> OpenSea </li>
-                <li> Rarible </li>
-                <li> SuperRare </li>
-                <li> Looksrare </li>
-                <li> Foundation </li>
-            </ol>
+            <p>
+                We use data provided <a href="https://www.similarweb.com/"> Similarweb </a> for five of the most popular NFT exchange platforms.
+                The platforms considered are name, <a href="https://opensea.io/"> OpenSea </a>, <a href="https://rarible.com/"> Rarible </a>, <a href="https://superrare.com/"> SuperRare </a>, <a href="https://looksrare.org/"> Looksrare </a> and <a href="https://foundation.app/"> Foundation </a>.
+            </p>
 
             <h2>
-                1. Monthly Traffic and total visits
+                1. Monthly traffic
             </h2>
+
+            <p>
+                The table belows gathers the monthly visits to the platforms for May, June and July.
+                It shows that 160 million visits where performed during that period.
+                OpenSea appears as the clear leader with a total of 140 million visits, i.e. 88% of it, followed by Foundation with 4% of the traffic.
+            </p>
 
             <table style={{ "min-width": "50em" }}>
                 
@@ -108,12 +119,12 @@ export const Article = () => {
                     </tr>
 
                     <tr>
-                        <td className="font-bold"> Share [%] </td>
-                        <td > 88 </td>
-                        <td> 3 </td>
-                        <td> 2 </td>
-                        <td> 3 </td>
-                        <td> 4 </td>
+                        <td className="font-bold"> Share </td>
+                        <td > 88% </td>
+                        <td> 3% </td>
+                        <td> 2% </td>
+                        <td> 3% </td>
+                        <td> 4% </td>
                         <td> </td>
                     </tr>
 
@@ -121,15 +132,16 @@ export const Article = () => {
 
             </table>
 
-            <p>
-                The total number of visits from May to July for the five platforms equals 160 million. 
-                An impressive 88% share of the visits are done on OpenSea.
-                Note further that since May, the number of monthly visits has decreased.
-            </p>
-
             <h2>
                 2. Mobile or Desktop
             </h2>
+
+            <p>
+                The users use mostly Desktop to visit the platforms, i.e. nearly 55% of the traffic.
+                Mobile Web traffic isn't lagging far behind with 45% of it.
+                On one hand, Looksrare is the platform that attracts most of its users on Desktops.
+                On the other hand, Rarible and SuperRare attract most of their traffic from mobiles.
+            </p>
 
             <table style={{ "min-width": "50em" }}>
                 
@@ -172,31 +184,37 @@ export const Article = () => {
             
             </table>
     
-            <p>
-                The users use mostly Desktop to visit the platforms.
-                But Mobile Web traffic isn't lagging far behind Desktop and account on average for 45% of the visits.
-                Note that Looksrare seems to benefit mostly from Desktop visits.
-            </p>
+
 
             <h2>
-                3. Gender and Age
+                3. Gender and age
             </h2>
 
-                <table style={{ "min-width": "50em" }}>
+            <p>
+                The majority of users are men.
+                It is especially the case for OpenSea with 70.17% of its users.
+                SuperRare appears to be the platforms attracting the most of female users with 37,73% of its traffic followed by Rarible with 35.87% of it.
+                Is there a correllation with mobile web traffic ?
+                On average, 36,67% of the traffic on the platforms comes from users aged between 25 and 34 years.
+                They are followed by users from 18 to 24 years old with 26.22% of the traffic.
+                In total, 80% of traffic comes from users less than 45 years old.
+            </p>
+
+            <table style={{ "min-width": "50em" }}>
                     
-                    <thead>
-                        <tr>
-                            <th colspan="2" className="text-center"> Platform </th>
-                            <th> OpenSea </th>
-                            <th> Rarible </th>
-                            <th> SuperRare </th>
-                            <th> Looksrare </th>
-                            <th> Foundation </th>
-                            <th> Weighted Average </th>
-                        </tr>
-                    </thead>
+                <thead>
+                    <tr>
+                        <th colspan="2" className="text-center"> Platform </th>
+                        <th> OpenSea </th>
+                        <th> Rarible </th>
+                        <th> SuperRare </th>
+                        <th> Looksrare </th>
+                        <th> Foundation </th>
+                        <th> Weighted Average </th>
+                    </tr>
+                </thead>
     
-                    <tbody>
+                <tbody>
    
                     <tr>
                         <td rowspan="2"> Gender </td>
@@ -284,125 +302,122 @@ export const Article = () => {
                 </tbody>
             
             </table>
-    
-            <p>
-                SuperRare is the platforms that attracts mosts female users, i.e. 37,73%, while OpenSea attracts mostly men, 70.17%.
-                On average, 36,67% of the traffic on the platform is done by users between 25 and 34 years old followed by users aged between 18 to 24 account for 26.22 % of the users.
-            </p>
-    
+        
             <h2>
                 4. Social networks
             </h2>
 
-                <table style={{ "min-width": "50em" }}>
-                    
-                    <thead>
-                        <tr>
-                            <th colspan="2" className="text-center"> Platform </th>
-                            <th> OpenSea </th>
-                            <th> Rarible </th>
-                            <th> SuperRare </th>
-                            <th> Looksrare </th>
-                            <th> Foundation </th>
-                            <th> Weighted Average </th>
-                        </tr>
-                    </thead>
-    
-                    <tbody>
-
-                        <tr>
-                            <td rowspan="7"> Social Traffic </td>
-                            <td className="font-bold"> Twitter [%] </td>
-                            <td> 73,63</td>
-                            <td> 54,95 </td>
-                            <td> 89,57 </td>
-                            <td> 90,10 </td>
-                            <td> 85,29 </td>
-                            <td className="font-bold"> 74,22 </td>
-                        </tr>
-
-                        <tr>
-                            <td> Youtube [%] </td>
-                            <td> 12,83 </td>
-                            <td> 21,54 </td>
-                            <td> 2,54 </td>
-                            <td> 5,87 </td>
-                            <td> 4,03 </td>
-                            <td> 12,41 </td>
-                        </tr>
-
-                        <tr>
-                            <td> Facebook [%] </td>
-                            <td> 4,52 </td>
-                            <td> 4,44 </td>
-                            <td> 2,12 </td>
-                            <td> 1,16 </td>
-                            <td> 3,02 </td>
-                            <td> 4,31 </td>
-                        </tr>
-
-                        <tr>
-                            <td> Instagram [%] </td>
-                            <td> 3,60 </td>
-                            <td> 10,20 </td>
-                            <td> 3,29 </td>
-                            <td> 1,10 </td>
-                            <td> 4,59 </td>
-                            <td> 3,77 </td>
-                        </tr>
-
-                        <tr>
-                            <td> Reddit [%] </td>
-                            <td> 1,34 </td>
-                            <td> 4,13 </td>
-                            <td> 1,77 </td>
-                            <td> 0,91 </td>
-                            <td>   </td>
-                            <td> 1,38 </td>
-                        </tr>
-
-                        <tr>
-                            <td> Whatsapp [%] </td>
-                            <td> </td>
-                            <td> </td>
-                            <td> </td>
-                            <td> </td>
-                            <td> 1,18 </td>
-                            <td> 0,04 </td>
-                        </tr>
-
-                        <tr>
-                            <td> Other [%] </td>
-                            <td> 4,08 </td>
-                            <td> 4,74 </td>
-                            <td> 0,71 </td>
-                            <td> 0,86  </td>
-                            <td> 1,89 </td>
-                            <td> 3,86 </td>
-                        </tr>
-
-                    </tbody>
-
-                </table>
-
             <p>
-                The clear leader in social network traffic is Twitter with nearly 75% of the shares.
-                It is followed by Youtube with 12,4% and Facebook with 4.3%. 
-                On one hand, SuperRare, Looksrare and Foundation benefit mostly from Twitter with more than 85% of their social network traffic comming from that platform.
-                In the other ahdn, Rarible is the platform that benefits the least from Twitter but seems better able to take advantage from Youtube than its competitors.
+                Most NFT users know that collections communicate mostly on Twitter.
+                However, it's impressive to realise it provides on average 75% of the social traffic for the platformes.
+                Youtube comes second with 12,4% of the social traffic and Facebook third with 4.3% of it.
+                On one hand, SuperRare, Looksrare and Foundation benefit mostly from Twitter with 85% of their social traffic.
+                In the other hand, Rarible is the platform that benefits the least from Twitter but seems better able to take advantage from Youtube better than its competitors.
             </p>
+
+            <table style={{ "min-width": "50em" }}>
+                    
+                <thead>
+                    <tr>
+                        <th colspan="2" className="text-center"> Platform </th>
+                        <th> OpenSea </th>
+                        <th> Rarible </th>
+                        <th> SuperRare </th>
+                        <th> Looksrare </th>
+                        <th> Foundation </th>
+                        <th> Weighted Average </th>
+                    </tr>
+                </thead>
+    
+                <tbody>
+
+                    <tr>
+                        <td rowspan="7"> Social Traffic </td>
+                        <td className="font-bold"> Twitter [%] </td>
+                        <td> 73,63</td>
+                        <td> 54,95 </td>
+                        <td> 89,57 </td>
+                        <td> 90,10 </td>
+                        <td> 85,29 </td>
+                        <td className="font-bold"> 74,22 </td>
+                    </tr>
+
+                    <tr>
+                        <td> Youtube [%] </td>
+                        <td> 12,83 </td>
+                        <td> 21,54 </td>
+                        <td> 2,54 </td>
+                        <td> 5,87 </td>
+                        <td> 4,03 </td>
+                        <td> 12,41 </td>
+                    </tr>
+
+                    <tr>
+                        <td> Facebook [%] </td>
+                        <td> 4,52 </td>
+                        <td> 4,44 </td>
+                        <td> 2,12 </td>
+                        <td> 1,16 </td>
+                        <td> 3,02 </td>
+                        <td> 4,31 </td>
+                    </tr>
+
+                    <tr>
+                        <td> Instagram [%] </td>
+                        <td> 3,60 </td>
+                        <td> 10,20 </td>
+                        <td> 3,29 </td>
+                        <td> 1,10 </td>
+                        <td> 4,59 </td>
+                        <td> 3,77 </td>
+                    </tr>
+
+                    <tr>
+                        <td> Reddit [%] </td>
+                        <td> 1,34 </td>
+                        <td> 4,13 </td>
+                        <td> 1,77 </td>
+                        <td> 0,91 </td>
+                        <td>   </td>
+                        <td> 1,38 </td>
+                    </tr>
+
+                    <tr>
+                        <td> Whatsapp [%] </td>
+                        <td> </td>
+                        <td> </td>
+                        <td> </td>
+                        <td> </td>
+                        <td> 1,18 </td>
+                        <td> 0,04 </td>
+                    </tr>
+
+                    <tr>
+                        <td> Other [%] </td>
+                        <td> 4,08 </td>
+                        <td> 4,74 </td>
+                        <td> 0,71 </td>
+                        <td> 0,86  </td>
+                        <td> 1,89 </td>
+                        <td> 3,86 </td>
+                    </tr>
+
+                </tbody>
+
+            </table>
 
             <h2>
                 Conclusion
             </h2>
 
             <p>
-                The present study shows that between May and July 2022 OpenSea is the clear leader in terms of traffic.
-                Indeed, they capture 88% of the traffic.
-                Twitter is the social network that brings most of the users on the platforms, i.e. nearly 75% of it.
-                The users are mostly men aged between 25 and 34 years old.
-                Desktop based traffic is leading over Mobile Web by 10%.
-                Let's see in December if the numbers have changed !
+                The present article focused on analysing traffic usage on 5 of the most popular NFT platforms between May and July 2022.
+                While many numbers where highlighted here are the following one you should remember.
+                First, OpenSea is the clear leader in terms of traffic with 88% of it.
+                Second, Twitter provides 75% of the social traffic to the platforms.
+                Third, the users are mostly men, nearly 70%, and 80% are less than 45 years old.
+                Fourth, Desktop based traffic is leading over Mobile Web by 10%, i.e. 55% vs 45% respectively.
+                Finally, I hope these number will help you in your Web3 journey, let's see in December if the numbers have changed !
             </p>
 
         </div>
